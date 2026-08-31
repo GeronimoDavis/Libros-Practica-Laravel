88578904a073d1bab8e5522bc05cfb2e06a2c25c
GeronimoDavis <geronimodavis@gmail.com>
2026-08-30 22:11:53 -0300

    Controladores

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//rutas de books
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+Route::get('/books', [BookController::class, 'store'])->name('books.store');
+Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
+
+
+ 
