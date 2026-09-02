ed1007c60961544d49b6c77e15966517183054d0
GeronimoDavis <geronimodavis@gmail.com>
2026-09-01 23:54:51 -0300

    Get libros

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     public function index()
     {
-        
+        $books = Book::with(['author', 'category'])->latest()->get();
+
+        return view('books.index', compact('books'));
     }
 
 }
